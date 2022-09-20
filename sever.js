dbc29792506bb24419bdead4c5f82115ae37e98a
levinhky <chansolle@gmail.com>
2022-09-20 10:20:48 +0700

    up

--- sever.js
+++ sever.js
@@ -20,7 +20,7 @@ dotenv.config();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods ","PUT, GET, POST, DELETE, OPTIONS")
+  res.header( 'Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS',)
   next();
 });
 
