f705d327852de0a4146bab5700d6fbaa8c7ffb13
levinhky <chansolle@gmail.com>
2022-09-20 10:33:27 +0700

    up

--- sever.js
+++ sever.js
@@ -17,12 +17,6 @@ app.use(cors());
 app.use(morgan('common'));
 dotenv.config();
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
-
 mongoose.connect(process.env.MONGODB_URL, () => {
   console.log('Database connect successfully!');
 }).catch(err => console.log(err));
