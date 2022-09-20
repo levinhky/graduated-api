22e96e1ec0b3e3c45c7c46dc7b21cf7607606361
levinhky <chansolle@gmail.com>
2022-09-20 10:46:19 +0700

    up

--- sever.js
+++ sever.js
@@ -26,6 +26,14 @@ app.use('/api/v2/categories', categoryRoute);
 app.use('/api/v2/products', productRoute);
 app.use('/api/v2/comments', commentRoute);
 
+  app.use( function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+
 app.listen(process.env.PORT || 8000, () => {
   console.log('Sever is running...!');
 })
