df5705030327de694d2f31b79168e756d68321ac
levinhky <chansolle@gmail.com>
2022-09-20 10:52:22 +0700

    up

--- sever.js
+++ sever.js
@@ -13,9 +13,14 @@ const commentRoute = require('./routes/comment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(morgan('common'));
 dotenv.config();
+const corsOptions ={
+  origin:'http://localhost:3000',
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGODB_URL, () => {
   console.log('Database connect successfully!');
