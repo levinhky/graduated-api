214d825f6c41d429148fbb4f3a63d8c4ae0345ba
levinhky <chansolle@gmail.com>
2022-09-20 10:49:30 +0700

    up

--- sever.js
+++ sever.js
@@ -27,7 +27,7 @@ app.use('/api/v2/products', productRoute);
 app.use('/api/v2/comments', commentRoute);
 
   app.use( function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
