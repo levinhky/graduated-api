b2e131af08015cab3479c8f629b48905151e2dc0
Kyle Wiley <chansolle@gmail.com>
2022-11-20 14:40:24 +0700

    up

--- controllers/productController.js
+++ controllers/productController.js
@@ -136,10 +136,10 @@ const productController = {
   // GET LENGTH
   getRows: async (req,res) => {
     try {
-     // const totalRows = await productSchema.aggregate([
-     //    { $count: "Total" }
-     //  ]);
-      const totalRows = await productSchema.countDocuments().exec();
+     const totalRows = await productSchema.aggregate([
+        { $count: "Total" }
+      ]);
+     //  const totalRows = await productSchema.countDocuments().exec();
      return res.status(200).json(totalRows);
     } catch (error) {
       console.log(error);
