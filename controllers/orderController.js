316945cc2d6243ddcf9e9aa286ddc1c74ae4ccc6
levinhky <chansolle@gmail.com>
2022-12-25 20:06:11 +0700

    up

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -87,8 +87,16 @@ const orderController = {
     },
      sendMail: async (req, res) => {
         try {
+            const currentdate = new Date();
             const to = req.query.to;
-            const html = req.query.html;
+            const html = `<div class="content">
+                <div class="welcome">
+                    Dear kyle,
+                    Thank you for your order from Pashi.
+                    If you have any question about your order,
+                    you can email us at <a href="mailto:pashion.shopping@gmail.com">pashion.shopping@gmail.com</a>
+                </div>
+            </div>`;
             await mailer.sendMail(to, 'Your Pashi order confirmation', html);
             return res.status(200).json('Send email successfully');
         } catch (error) {
